70fead0609fb3a29235ca1d8c6e56ac886209b6d
Fredrik Furufors <furufors@gmail.com>
2022-02-18 21:58:47 +0100

    Added day 2

--- day02/day2p1.hs
+++ day02/day2p1.hs
@@ -1,4 +1,34 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+data Direction = U | D | L | R
+
 main :: IO ()
-main = undefined
+main = interact $ show . solve . map parsein . lines
+
+solve :: [[Direction]] -> [Integer]
+solve dss = solve2 5 dss
+    where
+        solve2 :: Integer -> [[Direction]] -> [Integer]
+        solve2 _ [      ] = []
+        solve2 a (ds:dss) = let i1 = foldl step a ds
+                            in i1:(solve2 i1 dss)
+
+step :: Integer -> Direction -> Integer
+step i L | i `elem` [1,4,7] = i
+step i R | i `elem` [3,6,9] = i
+step i U | i `elem` [1,2,3] = i
+step i D | i `elem` [7,8,9] = i
+step i R = i + 1
+step i L = i - 1
+step i D = i + 3
+step i U = i - 3
+
+parsein :: String -> [Direction]
+parsein = map toDirection
+
+toDirection :: Char -> Direction
+toDirection 'U' = U
+toDirection 'D' = D
+toDirection 'L' = L
+toDirection 'R' = R
+toDirection  c  = error $ "missing char: " ++ [c]
--- day02/day2p2.hs
+++ day02/day2p2.hs
@@ -1,4 +1,44 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+data Direction = U | D | L | R
+
 main :: IO ()
-main = undefined
+main = interact $ show . solve . map parsein . lines
+
+solve :: [[Direction]] -> [Integer]
+solve dss = solve2 5 dss
+    where
+        solve2 :: Integer -> [[Direction]] -> [Integer]
+        solve2 _ [      ] = []
+        solve2 a (ds:dss) = let i1 = foldl step a ds
+                            in i1:(solve2 i1 dss)
+
+step :: Integer -> Direction -> Integer
+step i L | i `elem` [1, 2, 5,10,13] = i
+step i R | i `elem` [1, 4, 9,12,13] = i
+step i U | i `elem` [5, 2, 1, 4, 9] = i
+step i D | i `elem` [5,10,13,12, 9] = i
+step i R = i + 1
+step i L = i - 1
+step 3  U = 1
+step 1  D = 3
+step 13 U = 11
+step 11 D = 13
+step i D = i + 4
+step i U = i - 4
+
+parsein :: String -> [Direction]
+parsein = map toDirection
+
+toDirection :: Char -> Direction
+toDirection 'U' = U
+toDirection 'D' = D
+toDirection 'L' = L
+toDirection 'R' = R
+toDirection  c  = error $ "missing char: " ++ [c]
+
+--     1
+--   2 3 4
+-- 5 6 7 8 9
+--   A B C
+--     D
