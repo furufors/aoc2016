d99aa5f103d8c916621bdad536f2ccea76b604f0
Fredrik Furufors <furufors@gmail.com>
2022-02-18 22:17:42 +0100

    Added day 3

--- day03/day3p1.hs
+++ day03/day3p1.hs
@@ -1,4 +1,13 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter id . map (valid . parsein) . lines
+
+parsein :: String -> (Int, Int, Int)
+parsein s = let (a:b:c:_) = map read . words $ s
+            in (a,b,c)
+
+valid :: (Int, Int, Int) -> Bool
+valid (a,b,c) =    a + b > c
+                && a + c > b
+                && b + c > a
--- day03/day3p2.hs
+++ day03/day3p2.hs
@@ -1,4 +1,18 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (transpose)
+import Data.List.Split (chunksOf)
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter id . map (valid . parsein) . parse
+
+parse :: String -> [[Int]]
+parse = concat . map transpose . chunksOf 3 . map (map read . words) . lines
+
+parsein :: [Int] -> (Int, Int, Int)
+parsein s = let (a:b:c:_) = s
+            in (a,b,c)
+
+valid :: (Int, Int, Int) -> Bool
+valid (a,b,c) =    a + b > c
+                && a + c > b
+                && b + c > a
