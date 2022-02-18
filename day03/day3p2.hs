c4d41bce5db3ee3aaddc864c94b2f186f2bc91a4
Fredrik Furufors <furufors@gmail.com>
2022-02-18 22:22:44 +0100

    Added day 3, cleanup

--- day03/day3p2.hs
+++ day03/day3p2.hs
@@ -3,16 +3,10 @@
 import Data.List (transpose)
 import Data.List.Split (chunksOf)
 main :: IO ()
-main = interact $ show . length . filter id . map (valid . parsein) . parse
+main = interact $ show . length . filter id . map valid . concat . map transpose . chunksOf 3 . map (map read . words) . lines
 
-parse :: String -> [[Int]]
-parse = concat . map transpose . chunksOf 3 . map (map read . words) . lines
-
-parsein :: [Int] -> (Int, Int, Int)
-parsein s = let (a:b:c:_) = s
-            in (a,b,c)
-
-valid :: (Int, Int, Int) -> Bool
-valid (a,b,c) =    a + b > c
-                && a + c > b
-                && b + c > a
+valid :: [Int] -> Bool
+valid (a:b:c:[]) =    a + b > c
+                   && a + c > b
+                   && b + c > a
+valid _ = error "A row contains more or less than three elements."
