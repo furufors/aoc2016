62135d4cacb871792f0492569487714eaddfecc1
Fredrik Furufors <furufors@gmail.com>
2022-02-20 20:36:00 +0100

    Added day 7

--- day07/day7p1.hs
+++ day07/day7p1.hs
@@ -1,4 +1,23 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List.Split (splitWhen)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter (id) . map parseIP . lines
+
+parseIP :: String -> Bool
+parseIP s = let (outside, inside) = inAndOut s
+            in any hasABBA inside && (not . any hasABBA) outside
+
+inAndOut :: String -> ([String], [String])
+inAndOut s = let pieces = splitWhen (\a -> a == '[' || a == ']') s
+                 odds = [pieces!!i | i <- [0..(length pieces - 1)], odd i]
+                 evens = [pieces!!i | i <- [0..(length pieces - 1)], even i]
+             in (odds, evens)
+
+hasABBA :: String -> Bool
+hasABBA ([])         = False
+hasABBA (a:[])       = False
+hasABBA (a:b:[])     = False
+hasABBA (a:b:c:[])   = False
+hasABBA (a:b:c:d:es) = (a == d && b == c && a /= b) || hasABBA (b:c:d:es)
--- day07/day7p2.hs
+++ day07/day7p2.hs
@@ -1,4 +1,28 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List.Split (splitWhen)
+type Pair = (Char, Char)
+
 main :: IO ()
-main = undefined
+main = interact $ show . length . filter (id) . map parseIP . lines
+
+parseIP :: String -> Bool
+parseIP s = let (outside, inside) = inAndOut s
+                abas = foldl hasABA [] outside
+                babs = foldl hasABA [] inside
+                matches = [() | (a,b) <- abas, elem (b,a) babs]
+            in length matches > 0
+
+inAndOut :: String -> ([String], [String])
+inAndOut s = let pieces = splitWhen (\a -> a == '[' || a == ']') s
+                 odds = [pieces!!i | i <- [0..(length pieces - 1)], odd i]
+                 evens = [pieces!!i | i <- [0..(length pieces - 1)], even i]
+             in (odds, evens)
+
+hasABA :: [Pair] -> String -> [Pair]
+hasABA ps ([])         = ps
+hasABA ps (a:[])       = ps
+hasABA ps (a:b:[])     = ps
+hasABA ps (a:b:c:es) = if (a == c && a /= b)
+                       then (a,b):(hasABA ps (b:c:es))
+                       else hasABA ps (b:c:es)
