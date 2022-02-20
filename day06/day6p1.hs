53de06715b258a32ad1feba60b09e98e19fdb236
Fredrik Furufors <furufors@gmail.com>
2022-02-20 19:09:47 +0100

    Added day 5

--- day06/day6p1.hs
+++ day06/day6p1.hs
@@ -1,4 +1,6 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (group, transpose, sort, sortBy)
+
 main :: IO ()
-main = undefined
+main = interact $ map (head . last . sortBy (\a b -> length a `compare` length b) . group . sort) . transpose . lines
