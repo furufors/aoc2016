aa28b8605bc55691eebdf266f0ccba489e47a298
Fredrik Furufors <furufors@gmail.com>
2022-02-20 12:25:53 +0100

    Added day 4

--- day04/day4p1.hs
+++ day04/day4p1.hs
@@ -1,4 +1,19 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.List (group, sort, sortBy)
+
 main :: IO ()
-main = undefined
+main = interact $ show . sum . map valid . map parsein . lines
+
+valid :: (String, String, Int) -> Int
+valid (a,b,c) = let groups = group . sort . filter (not . (`elem` ['0'..'9'])) . filter (/='-') $ a
+                    groupCount = map (\a -> (length a, head a)) groups
+                    -- Primary higher number, secondary letter order
+                    sorted = sortBy (\a b -> fst b `compare` fst a <> snd a `compare` snd b) groupCount
+                    letterFreq = map snd sorted
+                in if take 5 letterFreq == b then c else 0
+
+parsein :: String -> (String, String, Int)
+parsein s = let (a,b) = span (/='[') s
+                is = filter (`elem` ['0'..'9']) a
+            in (a, take 5 . drop 1 $ b, read is)
