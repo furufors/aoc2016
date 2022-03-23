b16d7660880105dcae7a4751a4829d2f2cc16b5b
Fredrik Furufors <furufors@gmail.com>
2022-03-23 11:56:22 +0100

    Added day 9

--- day09/day9p1.hs
+++ day09/day9p1.hs
@@ -1,4 +1,13 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+main = interact $ show . length . decompress . head . lines
+
+decompress :: String -> String
+decompress ('(':ss) = let inst  = takeWhile (/= ')') ss
+                          rest  = drop 1 $ dropWhile (/= ')') ss
+                          count = read $ takeWhile (`elem` ['0'..'9']) inst
+                          reps  = read . drop 1 $ dropWhile (`elem` ['0'..'9']) inst
+                      in (concat $ replicate reps (take count rest)) ++ (decompress (drop count rest))
+decompress (s:ss) = s:(decompress ss)
+decompress [] = []
--- day09/day9p2.hs
+++ day09/day9p2.hs
@@ -1,4 +1,14 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
 main :: IO ()
-main = undefined
+--main = interact $ show . length . decompress . head . lines
+main = interact $ unlines . map (show . decompress) . lines
+
+decompress :: String -> Int
+decompress ('(':ss) = let inst  = takeWhile (/= ')') ss
+                          rest  = drop 1 $ dropWhile (/= ')') ss
+                          count = read $ takeWhile (`elem` ['0'..'9']) inst
+                          reps  = read . drop 1 $ dropWhile (`elem` ['0'..'9']) inst
+                      in reps * (decompress $ take count rest) + (decompress (drop count rest))
+decompress (s:ss) = 1 + decompress ss
+decompress [] = 0
