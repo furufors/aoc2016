b210655114534e98f000415eaf06423a6836c28b
Fredrik Furufors <furufors@gmail.com>
2022-11-22 22:47:36 +0100

    Added day 14

--- day14/day14p1.hs
+++ day14/day14p1.hs
@@ -1,4 +1,28 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Digest.Pure.MD5
+import Data.List (isInfixOf)
+import qualified Data.ByteString.Lazy.Char8 as C8
+
 main :: IO ()
-main = undefined
+main = putStrLn . show . fst $ (filter isKey generator) !! 63
+
+generator :: [(Int, String)]
+generator = [(i, show . md5 . C8.pack $ "jlmsuwbz" ++ show i) | i <- [0..]]
+
+isKey :: (Int, String) -> Bool
+isKey hash = case maybeFirstTriplet (snd hash) of
+    Just c -> fiveInNext1000 c hash
+    Nothing -> False
+
+maybeFirstTriplet :: String -> Maybe Char
+maybeFirstTriplet [] = Nothing
+maybeFirstTriplet (x:[]) = Nothing
+maybeFirstTriplet (x:y:[]) = Nothing
+maybeFirstTriplet (x:y:z:ss) = if x == y && x == z then Just x else maybeFirstTriplet (y:z:ss)
+
+fiveInNext1000 :: Char -> (Int, String) -> Bool
+fiveInNext1000 c (i,s) = any (numRepeat 5) . zip (repeat c) . take 1000 . drop (i+1) $ generator
+
+numRepeat :: Int -> (Char, (Int, String)) -> Bool
+numRepeat i (c,(_,s)) = isInfixOf (take i $ repeat c) s
