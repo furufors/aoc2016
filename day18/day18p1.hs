a912ae4f92f65c7842fc6c902090da281a992174
Fredrik Furufors <furufors@gmail.com>
2022-11-29 21:12:29 +0100

    Added day 18

--- day18/day18p1.hs
+++ day18/day18p1.hs
@@ -1,4 +1,35 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+
+data Plate = Safe | Trap deriving Show
 main :: IO ()
-main = undefined
+main = interact $ show . calculate . (\s -> iterate addRow [s] !! 39) . map parse . head . lines
+
+addRow :: [[Plate]] -> [[Plate]]
+addRow ps = ps ++ [newRow $ last ps]
+
+newRow :: [Plate] -> [Plate]
+newRow p = checkTile $ [Safe] ++ p ++ [Safe]
+
+checkTile :: [Plate] -> [Plate]
+checkTile (a:b:c:ds) =
+    let rest = checkTile (b:c:ds)
+    in case (a,b,c) of
+        (Trap,Trap,Safe) -> Trap:rest
+        (Safe,Trap,Trap) -> Trap:rest
+        (Safe,Safe,Trap) -> Trap:rest
+        (Trap,Safe,Safe) -> Trap:rest
+        otherwise        -> Safe:rest
+checkTile _ = []
+
+calculate :: [[Plate]] -> Int
+calculate = sum . map (sum . map toInt)
+
+toInt :: Plate -> Int
+toInt Safe = 1
+toInt Trap = 0
+
+parse :: Char -> Plate
+parse '.' = Safe
+parse '^' = Trap
+parse x   = error $ "Plate descrition " ++ [x] ++ " is not valid"
--- day18/day18p2.hs
+++ day18/day18p2.hs
@@ -1,4 +1,36 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+
+data Plate = Safe | Trap deriving (Show, Eq)
 main :: IO ()
-main = undefined
+main = interact $ show . calculate 400000 0 . map parse . head . lines
+
+calculate :: Int -> Int -> [Plate] -> Int
+calculate 0 n _  = n
+calculate i n ps = calculate (i-1) (n + (sum . map toInt) ps) (newRow ps)
+
+addRow :: [[Plate]] -> [[Plate]]
+addRow ps = ps ++ [newRow $ last ps]
+
+newRow :: [Plate] -> [Plate]
+newRow p = checkTile $ [Safe] ++ p ++ [Safe]
+
+checkTile :: [Plate] -> [Plate]
+checkTile (a:b:c:ds) =
+    let rest = checkTile (b:c:ds)
+    in case (a,b,c) of
+        (Trap,Trap,Safe) -> Trap:rest
+        (Safe,Trap,Trap) -> Trap:rest
+        (Safe,Safe,Trap) -> Trap:rest
+        (Trap,Safe,Safe) -> Trap:rest
+        otherwise        -> Safe:rest
+checkTile _ = []
+
+toInt :: Plate -> Int
+toInt Safe = 1
+toInt Trap = 0
+
+parse :: Char -> Plate
+parse '.' = Safe
+parse '^' = Trap
+parse x   = error $ "Plate descrition " ++ [x] ++ " is not valid"
