1438572b7aaa244244051cfd639a5e040f7ca815
Fredrik Furufors <furufors@gmail.com>
2022-12-10 18:21:12 +0100

    Added day 19

--- day19/day19p1.hs
+++ day19/day19p1.hs
@@ -1,4 +1,13 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Sequence
+
 main :: IO ()
-main = undefined
+main = interact $ show . game . read . head . lines
+
+game :: Int -> Int
+game n = gogogo . fromList $ Prelude.zip [1..n] (repeat 1)
+
+gogogo :: Seq (Int,Int) -> Int
+gogogo ((id,n):<|Empty) = id
+gogogo ((id1,n1):<|(_,n2):<|rest) = gogogo $ rest |> (id1,n1+n2)
--- day19/day19p2.hs
+++ day19/day19p2.hs
@@ -1,4 +1,18 @@
 #!/usr/bin/env stack
 -- stack --resolver lts-18.18 script
+import Data.Sequence (Seq, (<|), (|>), (><))
+import qualified Data.Sequence as Seq
+
 main :: IO ()
-main = undefined
+main = interact $ show . game . read . head . lines
+
+game :: Int -> Int
+game n = gogogo $ Seq.fromList [1..n]
+
+gogogo :: Seq Int -> Int
+gogogo (id Seq.:<| Seq.Empty) = id
+gogogo rest = let half  = (Seq.length rest) `div` 2
+                  left  = Seq.take half rest
+                  right = Seq.drop (half + 1) rest
+                  new = left >< right
+              in gogogo $ (Seq.drop 1 new) >< (Seq.take 1 new)
